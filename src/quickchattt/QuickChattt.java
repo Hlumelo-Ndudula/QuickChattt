b68fb358e26bdd699085e02c62a2dc4b298bf59f
Student <Student@LabVM2047644.isgpl30zdifudebo0gsw0ixpkf.jnbx.internal.cloudapp.net>
2026-05-24 19:59:44 +0200

    added string

---
+++ src/quickchattt/QuickChattt.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quickchattt;
+import java.util.*;
+/**
+ *
+ * @author Student
+ */
+public class QuickChattt {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new
+            Scanner (System.in);
+        System.out.println("Welcome to QuickChattt.");
+        System.out.print("Enter no. of messages:");
+        int n = sc.nextInt();sc.nextLine();
+        for (int i=1;i<=n;i++){
+            System.out.print("Recipient name:");
+            String r=sc.nextLine();
+            System.out.print("Your message:");
+            String m=sc.sc.nextLine();
+            
+        }
+    }
+    
+}
